9a90b974c8984231132e6efc3fc3ba09e545b742
bahadirdemircode <bahadirdemir@outlook.com>
2025-03-28 21:53:25 +0300

    Fix: Added dj-database-url dependency

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+
 echo "Running database migrations..."
 python manage.py migrate
+
 echo "Starting Gunicorn..."
-gunicorn blogapp.wsgi:application --bind 0.0.0.0:8000
+gunicorn blogapp.wsgi:application --bind 0.0.0.0:${PORT}
